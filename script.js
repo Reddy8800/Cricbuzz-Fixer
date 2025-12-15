ce74e71072b05c4bad85b5720d5c84bb49d39742
codearvind07 <arvindfb07@gmail.com>
2025-12-15 10:56:56 +0530

    Added full project

---
+++ script.js
@@ -0,0 +1,14 @@
+// ---- UPDATE THESE VALUES ---- //
+
+document.getElementById("channelLogo").src = "logo.jpg";  
+document.getElementById("channelName").innerText = "𝐂𝐑𝐈𝐂𝐁𝐔𝐙𝐙 𝐅𝐈𝐗𝐄𝐑™"; 
+document.getElementById("subscriberCount").innerText = "49019+";
+
+// Want auto-fetch subscribers?  
+// Tell me your channel username: @yourchannelname
+
+
+// ---- AUTO REDIRECT AFTER 5 SECONDS ---- //
+setTimeout(function () {
+    window.location.href = "https://t.me/+KOkPJ11vT_MyMWU1"; 
+}, 20000);
